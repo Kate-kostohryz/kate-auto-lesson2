f5aa513e46a233c5e1e359032a9277bd7125b356
Kate-kostohryz <glazkova@qamadness.com>
2025-07-20 16:57:48 +0300

    added code to do homework

---
+++ lesson3/task1.js
@@ -0,0 +1,11 @@
+let a = Math.floor(Math.random() * 10);
+let b = Math.floor(Math.random() * 10);
+let c = Math.floor(Math.random() * 10);
+
+console.log(a, b, c);
+console.log(a + b);
+console.log(b - c);
+console.log(a * c);
+console.log(c % b);
+console.log(a ** b);
+
---
+++ lesson3/task2.js
@@ -0,0 +1,5 @@
+let age = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
+let isAdult = age >= 18.
+
+console.log(age);
+console.log(isAdult); 
